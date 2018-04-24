82b02bc4cf8565a6c5630f53223d33457824ba97
Phyo Phyo Lwin <1041577@fairprice.sg>
2018-04-24 17:05:07 +0800

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 let socket = io();
 
+function scrollToBottom(){
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    //Heights
+    var clientHeight = messages.prop('clientHeight');  //to fetch the cross-browser way of property
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function (){
     console.log('Connected to server from client');
 
@@ -36,7 +53,7 @@ socket.on('newMessage', function(message){
 
     jQuery('#messages').append(html);
 
-    
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
@@ -53,7 +70,7 @@ socket.on('newLocationMessage', function(message){
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // //setting <a target="_blank" tells the browser to open new tab instead of opening on the same page.
     // var a = jQuery('<a target="_blank">My current location</a>');
